9cb9e6d6d88bfcd66cc5b2614e07ea293b75ab87
Manuel Ferreira <manuel.f.ferreira@tecnico.ulisboa.pt>
2024-11-17 14:25:18 +0000

    update

--- auxiliar-scripts/camera-gen.py
+++ auxiliar-scripts/camera-gen.py
@@ -7,10 +7,10 @@ with open(path, 'r') as f:
     data = json.load(f)
 
 # Get the data for object 55
-object_55_data = data['0']
+object_55_data = data['1']
 
 # Replicate object 55 data for objects 56 to 88
-for i in range(1, 505):
+for i in range(0, 549):
     data[str(i)] = object_55_data
 
 # Write the updated data back to the JSON file
